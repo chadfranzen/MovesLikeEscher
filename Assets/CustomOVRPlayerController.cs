d31cacb14373fdd02e396959082cf34ae564293c
Franzen <cdfranz2@illinois.edu>
2016-10-23 12:19:51 -0500

    Import Chad's stuff

---
+++ Assets/CustomOVRPlayerController.cs
@@ -0,0 +1,387 @@
+﻿/************************************************************************************
+
+Copyright   :   Copyright 2014 Oculus VR, LLC. All Rights reserved.
+
+Licensed under the Oculus VR Rift SDK License Version 3.3 (the "License");
+you may not use the Oculus VR Rift SDK except in compliance with the License,
+which is provided at the time of installation or download, or which
+otherwise accompanies this software in either electronic or hard copy form.
+
+You may obtain a copy of the License at
+
+http://www.oculus.com/licenses/LICENSE-3.3
+
+Unless required by applicable law or agreed to in writing, the Oculus VR SDK
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+************************************************************************************/
+
+using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// Controls the player's movement in virtual reality.
+/// </summary>
+[RequireComponent(typeof(CharacterController))]
+public class CustomOVRPlayerController : MonoBehaviour
+{
+    /// <summary>
+    /// The rate acceleration during movement.
+    /// </summary>
+    public float Acceleration = 0.1f;
+
+    /// <summary>
+    /// The rate of damping on movement.
+    /// </summary>
+    public float Damping = 0.3f;
+
+    /// <summary>
+    /// The rate of additional damping when moving sideways or backwards.
+    /// </summary>
+    public float BackAndSideDampen = 0.5f;
+
+    /// <summary>
+    /// The force applied to the character when jumping.
+    /// </summary>
+    public float JumpForce = 0.3f;
+
+    /// <summary>
+    /// The rate of rotation when using a gamepad.
+    /// </summary>
+    public float RotationAmount = 1.5f;
+
+    /// <summary>
+    /// The rate of rotation when using the keyboard.
+    /// </summary>
+    public float RotationRatchet = 45.0f;
+
+    /// <summary>
+    /// If true, reset the initial yaw of the player controller when the Hmd pose is recentered.
+    /// </summary>
+    public bool HmdResetsY = false;
+
+    /// <summary>
+    /// If true, tracking data from a child OVRCameraRig will update the direction of movement.
+    /// </summary>
+    public bool HmdRotatesY = false;
+
+    protected CharacterController Controller = null;
+    protected OVRCameraRig CameraRig = null;
+
+    private float MoveScale = 1.0f;
+    private Vector3 MoveThrottle = Vector3.zero;
+    private float FallSpeed = 0.0f;
+    private OVRPose? InitialPose;
+    private float InitialYRotation = 0.0f;
+    private float MoveScaleMultiplier = 1.0f;
+    private float RotationScaleMultiplier = 1.0f;
+    private bool SkipMouseRotation = false;
+    private bool HaltUpdateMovement = false;
+    private bool prevHatLeft = false;
+    private bool prevHatRight = false;
+    private float SimulationRate = 60f;
+
+    Transform currentRamp = null;
+
+
+    void Start()
+    {
+        // Add eye-depth as a camera offset from the player controller
+        var p = CameraRig.transform.localPosition;
+        p.z = OVRManager.profile.eyeDepth;
+        CameraRig.transform.localPosition = p;
+    }
+
+    void Awake()
+    {
+        Controller = gameObject.GetComponent<CharacterController>();
+
+        if (Controller == null)
+            Debug.LogWarning("OVRPlayerController: No CharacterController attached.");
+
+        // We use OVRCameraRig to set rotations to cameras,
+        // and to be influenced by rotation
+        OVRCameraRig[] CameraRigs = gameObject.GetComponentsInChildren<OVRCameraRig>();
+
+        if (CameraRigs.Length == 0)
+            Debug.LogWarning("OVRPlayerController: No OVRCameraRig attached.");
+        else if (CameraRigs.Length > 1)
+            Debug.LogWarning("OVRPlayerController: More then 1 OVRCameraRig attached.");
+        else
+            CameraRig = CameraRigs[0];
+
+        InitialYRotation = transform.rotation.eulerAngles.y;
+    }
+
+    void OnEnable()
+    {
+        OVRManager.display.RecenteredPose += ResetOrientation;
+
+        if (CameraRig != null)
+        {
+            CameraRig.UpdatedAnchors += UpdateTransform;
+        }
+    }
+
+    void OnDisable()
+    {
+        OVRManager.display.RecenteredPose -= ResetOrientation;
+
+        if (CameraRig != null)
+        {
+            CameraRig.UpdatedAnchors -= UpdateTransform;
+        }
+    }
+
+    protected virtual void Update()
+    {
+        Ray ray = new Ray(transform.position, -transform.up * 3);
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, -transform.up * 3, Color.red);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawRay(transform.position, hit.normal * 10);
+            if (hit.normal != transform.up)
+            {
+                if (hit.transform.gameObject.GetComponent("Ramp") != null)
+                {
+                    currentRamp = hit.transform;
+                }
+                Vector3 rotateVector = currentRamp.right;
+                Vector3 rampUp = currentRamp.up;
+                float rotateDegrees = Vector3.Angle(transform.up, hit.normal);
+                if (Vector3.Angle(hit.normal, rampUp) < Vector3.Angle(transform.up, rampUp))
+                {
+                    rotateDegrees *= -1;
+                }
+                // Very hacky way of interpolating motion because I don't know how to Unity
+                Quaternion oldRotation = transform.rotation;
+                transform.RotateAround(transform.position, rotateVector, rotateDegrees);
+                Quaternion newRotation = transform.rotation;
+                transform.rotation = Quaternion.Slerp(oldRotation, newRotation, Time.deltaTime * 10f);
+
+            }
+
+        }
+
+
+        UpdateMovement();
+
+        Vector3 moveDirection = Vector3.zero;
+
+        float motorDamp = (1.0f + (Damping * SimulationRate * Time.deltaTime));
+
+        MoveThrottle.x /= motorDamp;
+        MoveThrottle.y = (MoveThrottle.y > 0.0f) ? (MoveThrottle.y / motorDamp) : MoveThrottle.y;
+        MoveThrottle.z /= motorDamp;
+
+        Vector3 globalMoveDirection = Vector3.zero;
+        globalMoveDirection += (MoveThrottle.x * transform.right);
+        globalMoveDirection += MoveThrottle.y * transform.up;
+        globalMoveDirection += MoveThrottle.z * transform.forward;
+        globalMoveDirection *= SimulationRate * Time.deltaTime;
+
+
+        // Offset correction for uneven ground
+        /*
+        float bumpUpOffset = 0.0f;
+
+        if (Controller.isGrounded && MoveThrottle.y <= transform.lossyScale.y * 0.001f)
+        {
+            bumpUpOffset = Mathf.Max(Controller.stepOffset, new Vector3(moveDirection.x, 0, moveDirection.z).magnitude);
+            moveDirection -= bumpUpOffset * Vector3.up;
+        }
+        
+
+        Vector3 predictedXZ = Vector3.Scale((Controller.transform.localPosition + moveDirection), new Vector3(1, 0, 1));
+        */
+
+        // Move contoller
+        Controller.Move(globalMoveDirection);
+
+        //Vector3 actualXZ = Vector3.Scale(Controller.transform.localPosition, new Vector3(1, 0, 1));
+
+        //if (predictedXZ != actualXZ)
+            //MoveThrottle += (actualXZ - predictedXZ) / (SimulationRate * Time.deltaTime);
+    }
+
+    public virtual void UpdateMovement()
+    {
+        if (HaltUpdateMovement)
+            return;
+        
+        MoveScale = 1.0f;
+
+        MoveScale *= SimulationRate * Time.deltaTime;
+
+        // Compute this for key movement
+        float moveInfluence = Acceleration * 0.1f * MoveScale * MoveScaleMultiplier;
+
+#if !UNITY_ANDROID // LeftTrigger not avail on Android game pad
+        moveInfluence *= 1.0f + OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+#endif
+
+        // Run!
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            moveInfluence *= 2.0f;
+
+        float rotateInfluence = SimulationRate * Time.deltaTime * RotationAmount * RotationScaleMultiplier;
+
+        Vector2 primaryAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+
+        if (primaryAxis.y > 0.0f)
+            MoveThrottle += (primaryAxis.y * transform.lossyScale.z * moveInfluence * Vector3.forward);
+
+        if (primaryAxis.y < 0.0f)
+            MoveThrottle += (Mathf.Abs(primaryAxis.y) * transform.lossyScale.z * moveInfluence * BackAndSideDampen * Vector3.back);
+
+        if (primaryAxis.x < 0.0f)
+            MoveThrottle += (Mathf.Abs(primaryAxis.x) * transform.lossyScale.x * moveInfluence * BackAndSideDampen * Vector3.left);
+
+        if (primaryAxis.x > 0.0f)
+            MoveThrottle += (primaryAxis.x * transform.lossyScale.x * moveInfluence * BackAndSideDampen * Vector3.right);
+
+        Vector2 secondaryAxis = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        float rotateAmt = secondaryAxis.x * rotateInfluence;
+        transform.RotateAround(transform.position, transform.up, rotateAmt);
+    }
+
+    /// <summary>
+    /// Invoked by OVRCameraRig's UpdatedAnchors callback. Allows the Hmd rotation to update the facing direction of the player.
+    /// </summary>
+    public void UpdateTransform(OVRCameraRig rig)
+    {
+        Transform root = CameraRig.trackingSpace;
+        Transform centerEye = CameraRig.centerEyeAnchor;
+        Debug.Log(root.position);
+        Debug.Log("Root: " + root.rotation.eulerAngles);
+        Debug.Log("CenterEye: " + centerEye.rotation.eulerAngles);
+        Debug.DrawRay(transform.position, transform.forward * 3, Color.blue);
+        Debug.DrawRay(transform.position, centerEye.transform.forward * 3, Color.yellow);
+
+
+        Vector3 diff = Vector3.Project(centerEye.transform.forward, transform.up.normalized);
+        Vector3 projection = centerEye.transform.forward - diff;
+        Debug.DrawRay(transform.position, projection * 3, Color.green);
+        float degrees = Vector3.Angle(transform.forward, projection);
+        Vector3 prevPos = root.position;
+        Quaternion prevRot = root.rotation;
+        transform.LookAt(transform.position + projection, transform.up);
+        root.position = prevPos;
+        root.rotation = prevRot;
+
+    }
+
+    /// <summary>
+    /// Jump! Must be enabled manually.
+    /// </summary>
+    public bool Jump()
+    {
+        if (!Controller.isGrounded)
+            return false;
+
+        MoveThrottle += new Vector3(0, transform.lossyScale.y * JumpForce, 0);
+
+        return true;
+    }
+
+    /// <summary>
+    /// Stop this instance.
+    /// </summary>
+    public void Stop()
+    {
+        Controller.Move(Vector3.zero);
+        MoveThrottle = Vector3.zero;
+        FallSpeed = 0.0f;
+    }
+
+    /// <summary>
+    /// Gets the move scale multiplier.
+    /// </summary>
+    /// <param name="moveScaleMultiplier">Move scale multiplier.</param>
+    public void GetMoveScaleMultiplier(ref float moveScaleMultiplier)
+    {
+        moveScaleMultiplier = MoveScaleMultiplier;
+    }
+
+    /// <summary>
+    /// Sets the move scale multiplier.
+    /// </summary>
+    /// <param name="moveScaleMultiplier">Move scale multiplier.</param>
+    public void SetMoveScaleMultiplier(float moveScaleMultiplier)
+    {
+        MoveScaleMultiplier = moveScaleMultiplier;
+    }
+
+    /// <summary>
+    /// Gets the rotation scale multiplier.
+    /// </summary>
+    /// <param name="rotationScaleMultiplier">Rotation scale multiplier.</param>
+    public void GetRotationScaleMultiplier(ref float rotationScaleMultiplier)
+    {
+        rotationScaleMultiplier = RotationScaleMultiplier;
+    }
+
+    /// <summary>
+    /// Sets the rotation scale multiplier.
+    /// </summary>
+    /// <param name="rotationScaleMultiplier">Rotation scale multiplier.</param>
+    public void SetRotationScaleMultiplier(float rotationScaleMultiplier)
+    {
+        RotationScaleMultiplier = rotationScaleMultiplier;
+    }
+
+    /// <summary>
+    /// Gets the allow mouse rotation.
+    /// </summary>
+    /// <param name="skipMouseRotation">Allow mouse rotation.</param>
+    public void GetSkipMouseRotation(ref bool skipMouseRotation)
+    {
+        skipMouseRotation = SkipMouseRotation;
+    }
+
+    /// <summary>
+    /// Sets the allow mouse rotation.
+    /// </summary>
+    /// <param name="skipMouseRotation">If set to <c>true</c> allow mouse rotation.</param>
+    public void SetSkipMouseRotation(bool skipMouseRotation)
+    {
+        SkipMouseRotation = skipMouseRotation;
+    }
+
+    /// <summary>
+    /// Gets the halt update movement.
+    /// </summary>
+    /// <param name="haltUpdateMovement">Halt update movement.</param>
+    public void GetHaltUpdateMovement(ref bool haltUpdateMovement)
+    {
+        haltUpdateMovement = HaltUpdateMovement;
+    }
+
+    /// <summary>
+    /// Sets the halt update movement.
+    /// </summary>
+    /// <param name="haltUpdateMovement">If set to <c>true</c> halt update movement.</param>
+    public void SetHaltUpdateMovement(bool haltUpdateMovement)
+    {
+        HaltUpdateMovement = haltUpdateMovement;
+    }
+
+    /// <summary>
+    /// Resets the player look rotation when the device orientation is reset.
+    /// </summary>
+    public void ResetOrientation()
+    {
+        if (HmdResetsY)
+        {
+            Vector3 euler = transform.rotation.eulerAngles;
+            euler.y = InitialYRotation;
+            transform.rotation = Quaternion.Euler(euler);
+        }
+    }
+}
+

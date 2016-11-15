63bd41e6caa1233983e2de74130f220a66846210
Dominic Le <ddle2@illinois.edu>
2016-11-15 14:44:22 -0600

    Teleport.cs maintains rotation when maintain orientation is selected

--- Assets/teleport.cs
+++ Assets/teleport.cs
@@ -1,42 +1,27 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System;
 
-public class teleport : MonoBehaviour {
+public class teleport : MonoBehaviour
+{
     public GameObject destination;
     public bool maintainOrientation = true;
 
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Teleporter entered...");
-        if (maintainOrientation) { 
-
-            if (Math.Abs(this.transform.forward.x - other.transform.forward.x) <= 1 &&
-                Math.Abs(this.transform.forward.y - other.transform.forward.y) <= 1 &&
-                Math.Abs(this.transform.forward.z - other.transform.forward.z) <= 1)
-            {
-                Vector3 offSet = this.transform.position - other.transform.position;
-
-                other.transform.position = destination.transform.position - offSet;
-
-            }
-        }else
-        {
-            other.transform.position = destination.transform.position + -1.5f*destination.transform.forward;
-
-            other.transform.eulerAngles = destination.transform.rotation.eulerAngles;
-            other.transform.Rotate(0, 180, 0);
-        }
     }
     void OnTriggerStay(Collider other)
     {
@@ -46,13 +31,22 @@ public class teleport : MonoBehaviour {
 
             if (Math.Abs(this.transform.forward.x - other.transform.forward.x) <= 1 &&
                 Math.Abs(this.transform.forward.y - other.transform.forward.y) <= 1 &&
-                Math.Abs(this.transform.forward.z - other.transform.forward.z) <= 1 &&
-                other.bounds.Contains(this.transform.position))
+                Math.Abs(this.transform.forward.z - other.transform.forward.z) <= 1)
             {
                 Vector3 offSet = this.transform.position - other.transform.position;
+                other.transform.position = destination.transform.position + offSet.z * destination.transform.right - offSet.x * destination.transform.forward;
 
-                other.transform.position = destination.transform.position - offSet;
+                Quaternion rOff = Quaternion.Inverse(this.transform.rotation) * other.transform.rotation;
+                other.transform.rotation = destination.transform.rotation * rOff;
+
             }
+        }
+        else
+        {
+            other.transform.position = destination.transform.position + -1.5f * destination.transform.forward;
+
+            other.transform.eulerAngles = destination.transform.rotation.eulerAngles;
+            other.transform.Rotate(0, 180, 0);
         }
     }
 

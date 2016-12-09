cc869c44a3ddd86c1a2fdb06a8e3a907ccd785e6
Myren <myren2@illinois.edu>
2016-12-08 20:08:58 -0600

    added level 4 into the game, it broke the movement and gravity. I added an incomplete teleport script, and background music. I replaced all ramps in the first 2 levels with segmented ramps.

---
+++ Assets/MoveToLevel.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToLevel : MonoBehaviour {
+    public GameObject player;
+    public GameObject one;
+    public GameObject two;
+    public GameObject three;
+    public GameObject four;
+	// Use this for initialization
+	void Start () {
+        two.SetActive(false);
+        //three.SetActive(false);
+        four.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("1"))
+        {
+            one.SetActive(true);
+            two.SetActive(false);
+            //three.SetActive(false);
+            four.SetActive(false);
+            player.transform.position = new Vector3(12.19f, 2.44f, 0);
+            player.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            two.SetActive(true);
+            one.SetActive(false);
+            //three.SetActive(false);
+            four.SetActive(false);
+            player.transform.position = new Vector3(12.19f, 2.44f, 0);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            //move to the third level
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            two.SetActive(false);
+            //three.SetActive(false);
+            one.SetActive(false);
+            four.SetActive(true);
+            player.transform.position = new Vector3(-0.55f, -94.63f,-121.4f);
+            player.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+}

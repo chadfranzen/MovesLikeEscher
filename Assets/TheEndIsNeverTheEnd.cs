fcf6aa118658e7a58ea40f8e9888679e89943cb2
Myren <myren2@illinois.edu>
2016-12-11 12:23:42 -0600

    added ramp tests

---
+++ Assets/TheEndIsNeverTheEnd.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class TheEndIsNeverTheEnd : MonoBehaviour {
+    public GameObject ob;
+	// Use this for initialization
+	void Start () {
+        ob.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        ob.SetActive(false);
+    }
+}
